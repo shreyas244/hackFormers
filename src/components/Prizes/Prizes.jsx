26d0a3c98e27cc17dd1070dfb10140c1989e1a97
shreyas244 <110598496+shreyas244@users.noreply.github.com>
2023-01-24 05:38:35 +0000

    Improved Prizes page

--- src/components/Prizes/Prizes.jsx
+++ src/components/Prizes/Prizes.jsx
@@ -7,9 +7,9 @@ export default function Prizes() {
       <div className="text-3xl text-white">Prizes Section</div>
       <div className="grid grid-cols-3 gap-10">
           <div className="w-full">
-            <div className='card bg-slate-600 rounded-md h-64'>
+            <div className='card bg-slate-600 rounded-md h-80'>
               <div className="flex justify-center align-center">
-                <div className="text-xl text-white">Image</div>
+              <img src="https://hackthisfall.tech/_nuxt/img/silver.3e9bbcb.svg" className="h-20 w-20" alt="" />
               </div>
               <div className="flex justify-center align-center bg-white rounded-md m-10 py-5">Second Prize</div>
               <div className="flex justify-center align-center text-5xl text-white">10K</div>
@@ -19,9 +19,9 @@ export default function Prizes() {
           
 
           <div className="w-full scale-125">
-            <div className='card bg-slate-600 rounded-md h-64'>
+            <div className='card bg-slate-600 rounded-md h-80'>
               <div className="flex justify-center align-center">
-                <div className="text-xl text-white">Image</div>
+                <img src="https://hackthisfall.tech/_nuxt/img/gold.e3b24e8.svg" className="h-20 w-20" alt="" />
               </div>
               <div className="flex justify-center align-center bg-white rounded-md m-10 py-5">First Prize</div>
               <div className="flex justify-center align-center text-5xl text-white">30K</div>
@@ -31,9 +31,9 @@ export default function Prizes() {
 
 
           <div className="w-full">
-            <div className='card bg-slate-600 rounded-md h-64'>
+            <div className='card bg-slate-600 rounded-md h-80'>
               <div className="flex justify-center align-center">
-                <div className="text-xl text-white">Image</div>
+              <img src="https://hackthisfall.tech/_nuxt/img/bronze.eb1941a.svg" className="h-20 w-20" alt="" />
               </div>
               <div className="flex justify-center align-center bg-white rounded-md m-10 py-5">Third Prize</div>
               <div className="flex justify-center align-center text-5xl text-white">5K</div>
